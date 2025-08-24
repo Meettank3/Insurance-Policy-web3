041d703cc6cf06f21054e49d8c2d640e587c8ff8
Meettank3 <125813530+Meettank3@users.noreply.github.com>
2025-08-24 23:49:46 +0530

    updated Policy struct as per need of IPFS

--- contracts/PolicyManagement.sol
+++ contracts/PolicyManagement.sol
@@ -3,34 +3,30 @@ pragma solidity ^0.8.28;
 
 // creating struct for policies
 struct Policy {
-    string metadata;
-    bool active;
+    //uint256 id;
+    address holder;
+    uint256 premiumAmount; // per-period or total (decide one)
+    uint256 startDate;
+    uint256 endDate;
+    bool active;  
 }
 
 // Example baseURI: "https://ipfs.io/ipfs/QmSGn7kmYExizUFTwUGdrTyAx4f6aT8PZcJ86m8CEVqXvZ/"
 contract PolicyManagement {
 
-    mapping(uint256 => Policy) public policies; // uint256 is policyId
-    mapping(address => uint256[]) public holderPolicies; 
-    string public baseURI;
+    // creating variables 
+    uint256 public nextPolicyId;
+    
+    mapping(uint256 => Policy[]) public policiesId;
+    mapping(address => mapping(uint256 => Policy)) public holderPolicies;
 
-    event PolicyCreated(uint256 indexed policyId, string metadataURI);
-
-    constructor(string memory _baseURI) {
-        baseURI = _baseURI; // Pass folder CID link ending with "/"
+    modifier onlyPolicyHolder(address _holder, uint256 _policyId) {
+        require(holderPolicies[_holder][_policyId].holder == msg.sender, "Only Policy Holder can call this function");
+        _;
     }
 
-    function createPolicy(
-        uint256 _policyId,
-        string memory _fileName
-    ) public {        
-        string memory fullURI = string(abi.encodePacked(baseURI, _fileName));
-
-        policies[_policyId] = Policy({
-            metadata: fullURI,
-            active: true
-        });
-
-        emit PolicyCreated(_policyId, fullURI);
+    function _exists(uint256 policyId) internal view returns (bool) { 
+        
     }
+
 }
